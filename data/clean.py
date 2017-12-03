37c67202df2f86f275ceecd82c2baa7556039d2f
Jason Ge <jasonge@Jasons-MacBook-Pro.local>
2017-12-02 20:20:16 -0500

    new

--- data/clean.py
+++ data/clean.py
@@ -9,3 +9,13 @@ Created on Sat Dec  2 19:53:29 2017
 import pandas as pd
 
 data = pd.read_csv("trump_tweets_new.csv")
+
+trump_dict = dict{}
+
+for index, row in data.iterrows():
+    text = row["text"]
+    words = text.split(" ")
+    for word in words:
+        w = word.lower()
+        
+        
