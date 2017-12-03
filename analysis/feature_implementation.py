db68658e42a7fcefd5a342ab6d5832888a97112f
zhoufrank <fzhou@college.harvard.edu>
2017-12-03 13:03:31 -0500

    change bag.run to bag.get

--- analysis/feature_implementation.py
+++ analysis/feature_implementation.py
@@ -7,7 +7,7 @@ f_1 = {"obama": 0.1, "trump": 0.3, "neil": 0.5, "kim": 0.1}
 f_2 = {"obama": 0.3, "trump": 0.1, "neil": 0.2, "kim": 0.4}
 
 # bag = bagofwords.BagOfWords()
-# bag_data = bag.run(["trump"])
+# bag_data = bag.get(["trump"])
 # print(bag_data)
 
 weights = [0.3, 0.7]
