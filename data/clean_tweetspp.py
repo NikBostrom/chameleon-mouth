4eb25fe0ad63810fc89ce23e59bb152be2a1e11c
zhoufrank <fzhou@college.harvard.edu>
2017-12-03 16:00:23 -0500

    trying to do clean_tweetspp.py but not really getting anywhere lol

--- data/clean_tweetspp.py
+++ data/clean_tweetspp.py
@@ -17,22 +17,22 @@ for person, file in zip(people, files):
     
     data = pd.read_csv("raw_tweets/" + file)
     
+
     for index, row in data.iterrows():
         text = row["text"]
         try:
+            
             words = text.split(" ")
             words = [convert(word) for word in words]
-            tagged_text = nltk.pos_tag(words)
-            for (word, POS) in tagged_text:
-                
-                if word not in dict_item.keys():
-                    dict_item[word] = 1
-                else:
-                    dict_item[word] += 1
-                if POS not in pos_dict.keys():
-                    pos_dict[POS] = 1
-                else:
-                    pos_dict[POS] += 1
+            
+            sentence = " ".join(words)
+            text = nltk.word_tokenize(sentence)
+            tagged_text = nltk.pos_tag(text)
+            POSs = [tag[1] for tag in tagged_text]
+
+            
+
+
         except:
             print(text)
             
