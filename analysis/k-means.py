9a5bb4dc72738fe47a733e662d8301f703798a7e
Jason Ge <jasonge@Jasons-MacBook-Pro.local>
2017-12-03 13:39:00 -0500

    k-means

---
+++ analysis/k-means.py
@@ -0,0 +1,20 @@
+# Python 3
+
+import pandas as pd
+#import bagofwords
+import os
+
+filepath = "../data/raw_tweets/"
+files = os.listdir(filepath)
+people = list(map(lambda x: x.split("_")[0], files))
+
+tweets = []
+
+for person, file in zip(people, files):
+    data = pd.read_csv(filepath + file)
+    data["person"] = person
+    data = data[["text", "person"]]
+    tweets.append(data)
+    
+tweets = pd.concat(tweets)
+
