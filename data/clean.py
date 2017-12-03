8eacf0e3ac0eaa5002bfc94b6538090bdf6e3e1c
Jason Ge <jasonge@Jasons-MacBook-Pro.local>
2017-12-02 20:42:33 -0500

    updated

--- data/clean.py
+++ data/clean.py
@@ -29,4 +29,4 @@ for index, row in data.iterrows():
 
 dictionary = pd.DataFrame({"word": list(trump_dict.keys()), "freq": list(trump_dict.values())})
 dictionary.sort(columns=["freq"], ascending= False)
-dictionary.to_csv("trump_dictionary.csv")
+dictionary.to_csv("trump_dictionary.csv", index = False)
