27c5f742aff7dd515af4216b3b61bd9edc2f5f06
NikBostrom <nbostrom@college.harvard.edu>
2017-12-02 21:51:22 -0500

    added kim

---
+++ data/clean_kim.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  2 21:36:07 2017
+
+@author: jasonge
+"""
+
+import pandas as pd
+import csv
+import nltk
+
+person = "kim"
+
+data = pd.read_csv('KimKardashian.csv')
+
+dict_item = dict()
+
+for index, row in data.iterrows():
+    text = row["text"]
+    try:
+        words = text.split(" ")
+        for word in words:
+            w = word.lower()
+            if w not in dict_item.keys():
+                dict_item[w] = 1
+            else:
+                dict_item[w] += 1
+    except:
+        print(text)
+        
+
+dictionary = pd.DataFrame({"word": list(dict_item.keys()), "freq": list(dict_item.values())})
+dictionary = dictionary.sort(columns=["freq"], ascending= False)
+dictionary["probability"] = dictionary["freq"] / sum(dictionary["freq"])
+dictionary.to_csv(person + "_dictionary.csv", index = False)
