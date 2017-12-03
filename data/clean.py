c2c6fdfc4ab8d7bebec49d0835fc27fbe8be63b8
Jason Ge <jasonge@Jasons-MacBook-Pro.local>
2017-12-02 20:03:51 -0500

    data

---
+++ data/clean.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  2 19:53:29 2017
+
+@author: jasonge
+"""
+
+import pandas as pd
+
+data = pd.read_csv("trump_tweets_new.csv")
