da8b46edb0e1251b47a7272c3f770b74bdd1e047
Jason Ge <jasonge@Jasons-MacBook-Pro.local>
2017-12-03 18:28:12 -0500

    update

--- analysis/misspellings.py
+++ analysis/misspellings.py
@@ -13,6 +13,13 @@ people = list(map(lambda x: x.split("_")[0], files))
 
 all_tweets = pd.read_csv("aggregate_tweets/all_tweets.csv")
 
+def misspelled_sentence(row):
+    tweet = row["text"]
+    words = text.split(" ")
+
+for person in people:
+    data = data[data["person"] == person]
+    
 
 print(people)
 tweets = []
@@ -32,6 +39,3 @@ for person, file in zip(people, files):
     data = pd.read_csv(filepath + file, encoding="latin1")
     data["num_missed"] = data.apply(lambda row: misspellings(row), axis=1)
     percentage[person] = sum(list(data["num_missed"])) / sum(list(data["freq"]))
-
-    
-        
