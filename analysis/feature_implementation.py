8d4b8e8737d897bf455bb74c8e268846b90e088f
NikBostrom <nbostrom@college.harvard.edu>
2017-12-04 15:59:22 -0500

    added to feature implementation

--- analysis/feature_implementation.py
+++ analysis/feature_implementation.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import bagofwords
+import averages
 
 f_1 = {"obama": 0.1, "trump": 0.3, "neil": 0.5, "kim": 0.05, "elon": 0.05}
 f_2 = {"obama": 0.3, "trump": 0.1, "neil": 0.2, "kim": 0.3, "elon": 0.1}
@@ -10,7 +11,12 @@ f_2 = {"obama": 0.3, "trump": 0.1, "neil": 0.2, "kim": 0.3, "elon": 0.1}
 # bag_data = bag.get(["trump"])
 # print("Bag data:", bag_data)
 
+aves = averages.Precalcd_Ave()
+ave_hashtags = aves.ave_num_words_per_tweet
+mean_word_lengths = aves.ave_of_ave_word_lengths_per_tweet
+std_word_lengths = aves.stdev_ave_word_len_per_tweet
 
+print(ave_hashtags, mean_word_lengths, std_word_lengths)
 
 tweet = "STEM education needs to be funded"
 word_list = tweet.split(" ")
