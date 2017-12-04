c7ee35c2fc5510313f1627dec175d49c9fbf6eea
zhoufrank <fzhou@college.harvard.edu>
2017-12-03 19:03:33 -0500

    working on knearest

--- analysis/k_nearest.py
+++ analysis/k_nearest.py
@@ -3,7 +3,22 @@
 import util
 
 class k_Nearest:
-    def __init__(self):
+    def __init__(self, datafile="all_tweets.csv"):
+    	self.tweets_all, self.tweets_POSs_all = util.gettweetsandPOSs()
+    	self.people = self.tweets_all.keys()
+
+    def wordsimilarity(self, tweet_new):
     	
+    	occurrences_all = np.zeros(len(tweets_all.values()))
+    	for i, tweet in enumerate(tweets_all.values()):
+    		occurrences = 0
+    		for word in tweet_new:
+    			occurrences += tweet.count(word)
+    		occurrences_all[i] = occurrences
+    	
+    	return occurrences_all
+
+    def compare(self, tweet_new):
+    	for 
 
 print(convert("test"))
