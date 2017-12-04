57dc78aa7e43f9755ac51c8cc0982b5683e66a2a
NikBostrom <nbostrom@college.harvard.edu>
2017-12-04 15:56:49 -0500

    added file to update if datasets are updated

---
+++ analysis/update.py
@@ -0,0 +1,12 @@
+import averages
+import misspellings
+import os
+'''
+Run this code if the data sets are updated
+'''
+a = Average()
+
+a.ave_nwords_per_tweet()
+a.ave_word_length()
+
+os.system("misspellings.py")
