37f125c422d6a8042cd93f33c409d045da8a6db4
Jason Ge <jasonge@Jasons-MacBook-Pro.local>
2017-12-04 22:31:02 -0500

    predictions

--- analysis/feature_implementation.py
+++ analysis/feature_implementation.py
@@ -18,46 +18,52 @@ aves = averages.Precalcd_Ave()
 bag = bagofwords.BagOfWords()
 
 test_set = pd.read_csv("../data/test_set.csv")
+test_set = test_set.sample(1000)
+test_set = test_set.reset_index()
 
 weights = [0.333,0.333,0.333]
 
 predictions = []
 
+num_total = len(test_set)
+
 for index, row in test_set.iterrows():
-    tweet = row["text"]
-
-    word_list = tweet.split(" ")
-
-    f_bag = bag.get(word_list)
-    f_misspelled = misspelled_distributions(tweet)
-    f_length = aves.probs(tweet)
-
-
-    features = [f_bag, f_misspelled, f_length]
-
-    f_w = zip(features, weights)
-
-    people = f_bag.keys()
-
-    final = dict()
-
-    for feature, weight in f_w:
-        for person in people:
-            if person not in final.keys():
-                final[person] = feature[person] * weight
-            else:
-                final[person] += feature[person] * weight
+    try:
+        tweet = row["text"]
     
-    p = max(final, key=final.get)
+        word_list = tweet.split(" ")
+    
+        f_bag = bag.get(word_list)
+        f_misspelled = misspelled_distributions(tweet)
+        f_length = aves.probs(tweet)
+    
+    
+        features = [f_bag, f_misspelled, f_length]
+    
+        f_w = zip(features, weights)
+    
+        people = f_bag.keys()
+    
+        final = dict()
+    
+        for feature, weight in f_w:
+            for person in people:
+                if person not in final.keys():
+                    final[person] = feature[person] * weight
+                else:
+                    final[person] += feature[person] * weight
+        
+        p = max(final, key=final.get)
+    except:
+        p = "error"
     predictions.append(p)
-    print("prediction: " + p + " actual: " + row["person"])
+    print("prediction: " + p + " actual: " + row["person"] + " progress: " + str(index/num_total))
     
 test_set["prediction"] = predictions
 test_set["correct"] = test_set["prediction"] == test_set["person"]
 
 correct = test_set[test_set["correct"] == True]
 num_correct = len(correct)
-num_total = len(test_set)
 test_set.to_csv("predictions.csv", index=False)
 print("Percent Correct" + (num_correct/num_total))
 
