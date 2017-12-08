589089ee77b201bc6aabba555adece34324ec36e
zhoufrank <fzhou@college.harvard.edu>
2017-12-07 21:57:31 -0500

    fixed print statements in knearest

--- analysis/k_nearest.py
+++ analysis/k_nearest.py
@@ -14,15 +14,9 @@ class k_Nearest:
     	(self.text_to_author, self.text_to_wordlist, self.text_to_POSlist, 
     		self.wordfreq, self.POSfreq) = util.getknntrainingtweets()
     	self.all_tweet_texts = list(self.text_to_author.keys())
-    	# self.tweets_all, self.tweets_POSs_all
     	self.no_of_tweets = len(self.text_to_author.keys())
-    	# self.people = self.tweets_all.keys()
 
     def all_similarities(self, tweet_new):
-
-    	# if len(self.tweets_all.values()) != len(self.tweets_POSs_all.values()):
-    	# 	print("Warning! Mismatched number of dataset tweets")
-
     	# Arrays for storage of similarity scores
     	(wordoccurrence_similarities, POSoccurrence_similarities, 
     		lenoftweet_similarities, avglenofword_similarities, 
@@ -34,21 +28,10 @@ class k_Nearest:
 
     	# Iterate over each tweet in the dataset (i.e. tweet_data)
     	for i, tweet_data_raw in enumerate(self.all_tweet_texts):
-    		
-    		# if (i % 1000) == 0:
-    		# 	print(i)
 
     		# Convert tweet in the training set to a wordlist and a POSlist
     		tweet_data = self.text_to_wordlist[tweet_data_raw]
     		tweet_data_POS = self.text_to_POSlist[tweet_data_raw]
-
-    		# if i == 10000:
-    		# 	print(tweet_new)
-    		# 	print(tweet_words_new)
-    		# 	print(tweet_POSs_new)
-    		# 	print(tweet_data_raw)
-    		# 	print(tweet_data)
-    		# 	print(tweet_data_POS)
 
     		# Calculate the number of times words in tweet_words_new occur in tweet_data
     		occurrences = sum([min(np.exp(-np.log(0.2*self.wordfreq[word])),1) for word in tweet_words_new if word in tweet_data])
@@ -85,10 +68,12 @@ class k_Nearest:
     		nummentions_similarities[i] = np.exp(-abs(nummentions_separation))
 
     	# Normalize
-    	wordoccurrence_similarities_norm = wordoccurrence_similarities/max(wordoccurrence_similarities)
-    	POSoccurrence_similarities_norm = POSoccurrence_similarities/max(POSoccurrence_similarities)
+    	if max(wordoccurrence_similarities) != 0:
+    		wordoccurrence_similarities = wordoccurrence_similarities/max(wordoccurrence_similarities)
+    	if max(POSoccurrence_similarities) != 0:
+    		POSoccurrence_similarities = POSoccurrence_similarities/max(POSoccurrence_similarities)
 
-    	return (wordoccurrence_similarities_norm, POSoccurrence_similarities_norm,
+    	return (wordoccurrence_similarities, POSoccurrence_similarities,
     		lenoftweet_similarities, avglenofword_similarities, 
     		numhashtags_similarities, numlinks_similarities, nummentions_similarities)
 
@@ -96,12 +81,9 @@ class k_Nearest:
     	
     	# Get the similarity vectors for each feature of interest
     	similarity_vectors = self.all_similarities(tweet_new)
-
-    	# print(np.shape(similarity_vectors))
-    	# print(max(similarity_vectors[0]))
     	
     	# The weightings of each feature
-    	weights = np.array([0.5, 0.25, 0.05, 0.05, 0.05, 0.05, 0.05])
+    	weights = np.array([0.75, 0.25, 0.0, 0.0, 0.0, 0.0, 0.0])
     	
     	# Perform linear combination of feature vectors
     	overall_similarities = np.zeros(self.no_of_tweets)
@@ -110,22 +92,6 @@ class k_Nearest:
 
     	# Get the indices of the k best tweets
     	ind = np.argpartition(-overall_similarities, k)[:k]
-    	
-    	# for i in ind:
-    	# 	print(overall_similarities[i])
-    	# 	print(similarity_vectors[0][i])
-    	# 	print(similarity_vectors[1][i])
-    	# 	print(similarity_vectors[2][i])
-    	# 	print(similarity_vectors[3][i])
-    	# 	print(similarity_vectors[4][i])
-    	# 	print(similarity_vectors[5][i])
-    	# 	print(similarity_vectors[6][i])
-    	# 	print(" ")
-
-
-    	# tweets = [self.all_tweet_texts[i] for i in ind]
-    	# print(tweets)
-    	# print([self.text_to_author[text] for text in tweets])
 
     	# Sort indices with most similar tweets first 
     	ind = sorted(ind, key= lambda x: overall_similarities[x], reverse=True)
@@ -141,9 +107,7 @@ class k_Nearest:
 
     	# Count the authorship and return most common author
     	authors_count = Counter(authors)
-    	# print(authors_count)
     	most_frequent_author = authors_count.most_common(1)[0][0]
-    	# print("Most frequent author: ", most_frequent_author)
 
     	# Because there might be ties, we iterate through the counter and
     	# identify any other authors which might also have the maximal
@@ -164,20 +128,3 @@ class k_Nearest:
     		for author in authors:
     			if author in most_frequent_authors:
     				return author
-
-
-# a = k_Nearest()
-# tweet = "Senate leaders' political games are handicapping the Supreme Court and judgeships across the country. http://ofa.bo/2dVVHno? #DoYourJob"
-# tweet1 = "With the great vote on Cutting Taxes, this could be a big day for the Stock Market - and YOU!"
-# tweet2 = "Putting Pelosi/Schumer Liberal Puppet Jones into office in Alabama would hurt our great Republican Agenda of low on taxes"
-# tweet3 = "Crest Velour Hoodie and Sweatpants http://thekidssupply.com "
-# b = a.get_k_nearest(tweet, k=12)
-# c = a.get_k_nearest(tweet1, k=12)
-# d = a.get_k_nearest(tweet2, k=12)
-# e = a.get_k_nearest(tweet3, k=12)
-
-# # 
-# print(b)
-# print(c)
-# print(d)
-# print(e)
