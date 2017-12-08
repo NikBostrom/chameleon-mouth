69bcc7e00074a98cb58495c60ec6e537104effc4
Jason Ge <jasonge@Jasons-MacBook-Pro.local>
2017-12-07 21:52:33 -0500

    new

--- analysis/single_tweet_knn.py
+++ analysis/single_tweet_knn.py
@@ -8,7 +8,7 @@ k = 10
 
 while(True):
 
-    user_input = input("Please Enter Your Tweet: ")
+    user_input = input("Please Enter Your Tweet (Control-C to Quit): ")
 
     prediction = knn.get_author_prediction(user_input, k)
 
