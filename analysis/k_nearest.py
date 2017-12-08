59f0d9a10bf1ac70a568dd4efffaa84b32c6bd3d
zhoufrank <fzhou@college.harvard.edu>
2017-12-07 21:38:09 -0500

    separate the file for testing out from the class

--- analysis/k_nearest.py
+++ analysis/k_nearest.py
@@ -101,7 +101,7 @@ class k_Nearest:
     	# print(max(similarity_vectors[0]))
     	
     	# The weightings of each feature
-    	weights = np.array([0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+    	weights = np.array([0.5, 0.25, 0.05, 0.05, 0.05, 0.05, 0.05])
     	
     	# Perform linear combination of feature vectors
     	overall_similarities = np.zeros(self.no_of_tweets)
@@ -181,33 +181,3 @@ class k_Nearest:
 # print(c)
 # print(d)
 # print(e)
-
-test_set = pd.read_csv("test_set_sample.csv")
-
-knn = k_Nearest()
-k = 10
-
-tweets = list(test_set["text"])
-predictions = [knn.get_author_prediction(tweet, k) for tweet in tweets]
-
-test_set["prediction"] = predictions
-test_set["correct"] = test_set["prediction"] == test_set["person"]
-
-correct = test_set[test_set["correct"] == True]
-num_correct = len(correct)
-test_set.to_csv("knn_predictions.csv", index=False)
-num_total = len(test_set)
-print("Percent Correct" + str(num_correct/num_total))
-
-people = list(set(test_set["person"]))
-
-for person in people:
-    temp = test_set[test_set["person"] == person]
-    num = len(temp)
-    correct = temp[temp["correct"] == True]
-    c = len(correct)
-    print("Person: " + person)
-    print("Tweets: " + str(num))
-    print("Number Correct: " + str(c))
-    print("Percentage Correct: " + str(c / num))
-    print()
