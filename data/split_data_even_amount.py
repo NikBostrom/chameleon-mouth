358756327763c420ee4d38767b764db7c5ebcd6e
zhoufrank <fzhou@college.harvard.edu>
2017-12-07 20:04:12 -0500

    add trumptweet.png

--- data/split_data_even_amount.py
+++ data/split_data_even_amount.py
@@ -17,9 +17,18 @@ import random
 #nltk.download("tagsets")
 #nltk.help.upenn_tagset()
 
-total_data = pd.read_csv("all_tweets.csv")
-people = list(data.person.unique())
-print(people)
+total_data = pd.read_csv("training_set.csv")
+
+people = list(total_data.person.unique())
+samples = []
+
+for person in people:
+	data_person = total_data[total_data["person"] == person]
+	sample = data_person.sample(n=1000, random_state=10)
+	samples.append(sample)
+
+equal_sample = pd.concat(samples)
+equal_sample.to_csv("knn_training_set.csv", index=False)
 
 # total_data["random"] = [random.random() for _ in range(len(total_data))]
 # test_set = total_data[total_data["random"] < 0.5]
